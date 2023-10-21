0b89901f53f68b433941c562403d7d0d761e9a6e
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-10-20 22:53:53 -0400

    0.29.0

--- apollo-client.ts
+++ apollo-client.ts
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "http://localhost:4000/graphql",
+    uri: "https://oxygen-dev-server-fzji.vercel.app/graphql",
     cache: new InMemoryCache(),
   });
 };
